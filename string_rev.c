cd7f7dd7fcaa9dddd087aeaf1c775572896d081e
Gauvrevprasad <gauvrevprasad@gmail.com>
2026-07-24 18:40:15 +0530

    just

--- string_rev.c
+++ string_rev.c
@@ -1,27 +1,5 @@
 #include<stdio.h>
 #include<string.h>
-int stringrev(char* a,int len)
-{
-    int i =0;
-    int j=len-1;
-   while(i<j)
-   {
-    a[i]=a[i]^a[j];
-    a[j]=a[i]^a[j];
-    a[i]=a[i]^a[j];
-    i++;
-    j--;
-   }
-   while(a[i]!=' ')
-   return 0;
-}
-
-
-
-
-
-
-
 int main()
 {
     char a[30];
